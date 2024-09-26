ad3f0139f014cba98db5cc8333259aafb3968c37
Vanessa Pereira <vanessappereira@outlook.pt>
2024-09-26 15:23:55 +0100

    minor update

--- Sessao4/Pessoa.java
+++ Sessao4/Pessoa.java
@@ -14,36 +14,38 @@ public class Pessoa {
         this.altura = altura;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
+    //Setters
     public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public String getEstado() {
-        return estado;
     }
 
     public void setEstado(String estado) {
         this.estado = estado;
     }
 
-    public int getIdade() {
-        return idade;
-    }
-
     public void setIdade(int idade) {
         this.idade = idade;
     }
 
-    public double getAltura() {
-        return altura;
-    }
-
     public void setAltura(double altura) {
         this.altura = altura;
+    }
+    //Getters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getAltura() {
+        return altura;
     }
 
 }
